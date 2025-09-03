9317573e27d845fa53667376131815fc01c35f15
NAYRA FONSECA DOS SANTOS <nayrasantos0842@digitalcollege.internal>
2025-09-02 22:00:37 -0300

    fazendo o usenavigate

--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,35 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom';
 import { Login } from './pages/Login.jsx';
 import { Cadastro } from './Cadastro.jsx';
 import { NotFound } from './pages/NotFound.jsx';
 
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+
+  {
+    path: "/home",
+    element: <App/>,
+  },
+
+  {
+    path: "/cadastro",
+    element: <Cadastro/>,
+  },
+
+  {
+    path: "*",
+    element: <NotFound/>,
+  },
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App/>} />
-        <Route path='/login' element={<Login />}/>
-         <Route path='/cadastro' element={<Cadastro />}/>
-         {/*400*/}
-         <Route path='*' element={<NotFound />}/>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
